45d731da1aa89a8ba05998f705c60318ddb7a5e1
Timur2311 <zayniddinov.programming@gmail.com>
2022-10-25 05:47:32 +0500

    saving_data changed

--- utils/saving_data.py
+++ utils/saving_data.py
@@ -5,35 +5,37 @@ from exam.models import Question
 from group_challenge.models import Challenge
 
 def saving_data():
-    workbook = pd.read_excel('media/questions/test.xlsx')
+    for i in range(40):
+        workbook = pd.read_excel('media/questions/test.xlsx',heet_name = f'{i+1}')
+        exam_title = workbook['name'].iloc[0]
+        stage = workbook['stage'].iloc[0]
+        tour = workbook['tour'].iloc[0]
+        
     # print(workbook.head())
-    questions_count = len(workbook['name'])
-    if len(workbook['question'])>0:
-        for i in range(questions_count):
-            exam_title = workbook['name'].iloc[i]
-            content = workbook['question'].iloc[i]
-            stage = workbook['stage'].iloc[i]
-            tour = workbook['tour'].iloc[i]
-            true_definition = workbook['comment'].iloc[i]
-            correct = workbook['correct'].iloc[i]            
-            incorrect1 = workbook['incorrect1'].iloc[i]
-            incorrect2 = workbook['incorrect2'].iloc[i]
-             
-            question  = Question.objects.create(content = content, stage = stage, tour = tour, true_definition=true_definition )    
-            question.create_exam(exam_title=exam_title)
-            question.add_question_options(content = correct, is_correct=True)
-            question.add_question_options(content = incorrect1)
-            question.add_question_options(content = incorrect2)
-            
-            
-            challenge_count = Challenge.objects.filter(stage = stage).count()
-            
-            if challenge_count==0:
-                challenge = Challenge.objects.create(stage =stage)
-                challenge.questions.add(question) 
-            elif challenge_count>0:
-                challenge = Challenge.objects.get(stage =stage)
-                challenge.questions.add(question) 
+        questions_count = len(workbook['question'])
+        if questions_count>0:
+            for i in range(0,questions_count,5):
+                content = workbook['question'].iloc[i]
+                incorrect1 = workbook['question'].iloc[i+1]
+                incorrect2 = workbook['question'].iloc[i+2]
+                correct = workbook['question'].iloc[i+3]            
+                true_definition = workbook['question'].iloc[i+4]
+                
+                question  = Question.objects.create(content = content, stage = stage, tour = tour, true_definition=true_definition )    
+                question.create_exam(exam_title=exam_title)
+                question.add_question_options(content = correct, is_correct=True)
+                question.add_question_options(content = incorrect1)
+                question.add_question_options(content = incorrect2)
+                
+                
+                challenge_count = Challenge.objects.filter(stage = stage).count()
+                
+                if challenge_count==0:
+                    challenge = Challenge.objects.create(stage =stage)
+                    challenge.questions.add(question) 
+                elif challenge_count>0:
+                    challenge = Challenge.objects.get(stage = stage)
+                    challenge.questions.add(question) 
                 
         
                 
