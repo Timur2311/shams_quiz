7404deded573d9734ebc51ccc773a022e073b225
Timur2311 <zayniddinov.programming@gmail.com>
2022-11-14 16:11:39 +0500

    admin panel changed

--- group_challenge/admin.py
+++ group_challenge/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 @admin.register(UserChallenge)
 class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
     list_display = [
         "user","opponent","challenge","is_random_opponent","created_challenge_message_id","created_challenge_chat_id",
     ]
